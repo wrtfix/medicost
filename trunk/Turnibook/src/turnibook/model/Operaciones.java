94d3c1529b7462252d52368c0275514f057bf194
wrtfix@gmail.com <wrtfix@gmail.com@d09ea622-3b30-78ad-f7ed-96db84a9408f>
2014-02-14 02:24:37 +0000

    Cambio de base de datos

--- trunk/Turnibook/src/turnibook/model/Operaciones.java
+++ trunk/Turnibook/src/turnibook/model/Operaciones.java
@@ -9,6 +9,7 @@ package turnibook.model;
 import turnibook.view.Agenda;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTable;
@@ -38,38 +39,43 @@ public class Operaciones {
 
     public boolean insertar(String sql, JTextField aviso) {
         boolean valor = true;
-        SqlLiteUtil.getInstance().conectar();
-        SqlLiteUtil.getInstance().insert(sql);
+        MySqlUtil.getInstance().conectar();
+        MySqlUtil.getInstance().insert(sql);
         aviso.setText("Los datos se guardaron correctamente");
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().desconectar();
         return valor;
     }
     
     public boolean updateTurno(String sql){
-        SqlLiteUtil.getInstance().conectar();
-        boolean res = SqlLiteUtil.getInstance().update(sql);
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().conectar();
+        boolean res = false;
+        res = MySqlUtil.getInstance().update(sql);
+        MySqlUtil.getInstance().desconectar();
         return res;
     }
     public void borrarTurno(String hora, String fecha, String id) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         String sql = "delete from turno where fecha = '" + fecha + "' and hora ='" + hora + "' and id_profesional='" + id + "'";
-        SqlLiteUtil.getInstance().query(sql);
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().query(sql);
+        MySqlUtil.getInstance().desconectar();
     }
 
     public ResultSet consultar(String sql) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado = null;
-        resultado = SqlLiteUtil.getInstance().query(sql);
+        resultado = MySqlUtil.getInstance().query(sql);
         return resultado;
     }
-
+    
+    public void closeDB(){
+        MySqlUtil.getInstance().desconectar();
+    }
+    
     public String IdHorario(String id_profesional) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado;
         String sql = "select id_horario from profesional where id_profesional='" + id_profesional + "'";
-        resultado = SqlLiteUtil.getInstance().query(sql);
+        resultado = MySqlUtil.getInstance().query(sql);
         String ret = "";
         try {
             if (resultado.next()) {
@@ -78,15 +84,15 @@ public class Operaciones {
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        SqlLiteUtil.getInstance().desconectar();
+        
         return ret;
     }
 
     public String ultimoIdHorario() {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado;
         String sql = "select max(id_horario) from horario";
-        resultado = SqlLiteUtil.getInstance().query(sql);
+        resultado = MySqlUtil.getInstance().query(sql);
         String ret = "";
         try {
             if (resultado.next()) {
@@ -95,56 +101,58 @@ public class Operaciones {
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().desconectar();
         return ret;
     }
 
     public boolean existeTurno(String fecha, String hora) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado;
-        resultado = SqlLiteUtil.getInstance().query("select hora from turno where fecha = '" + fecha + "' and hora ='" + hora + "'");
+        resultado = MySqlUtil.getInstance().query("select hora from turno where fecha = '" + fecha + "' and hora ='" + hora + "'");
         try {
             if (resultado.next()) {
-                SqlLiteUtil.getInstance().desconectar();
+                MySqlUtil.getInstance().desconectar();
                 return true;
             } else {
-                SqlLiteUtil.getInstance().desconectar();
+                MySqlUtil.getInstance().desconectar();
                 return false;
 
             }
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().desconectar();
         return false;
     }
 
     public ResultSet getTurnos(String fecha, String id_profesional) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado = null;
-        resultado = SqlLiteUtil.getInstance().query("select * from turno where fecha = '" + fecha + "' and id_profesional='" + id_profesional + "'");
-        SqlLiteUtil.getInstance().desconectar();
+        resultado = MySqlUtil.getInstance().query("select * from turno where fecha = '" + fecha + "' and id_profesional='" + id_profesional + "'");
+        MySqlUtil.getInstance().desconectar();
         return resultado;
     }
 
     public String getIntervalo(String dia, String id_horario) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado = null;
         String intervalo = "";
         try {
-            resultado = SqlLiteUtil.getInstance().query("select " + dia + "_desde, " + dia + "_hasta , intervalo from horario where id_horario=" + id_horario);
+            resultado = MySqlUtil.getInstance().query("select " + dia + "_desde, " + dia + "_hasta , intervalo from horario where id_horario=" + id_horario);
             if (resultado.next()) {
-                intervalo = resultado.getString(dia + "_desde") + "-" + resultado.getString(dia + "_hasta") + "-" + resultado.getString("intervalo");
+                if (!resultado.getString(dia + "_desde").equals("0")){
+                    intervalo = resultado.getString(dia + "_desde") + "-" + resultado.getString(dia + "_hasta") + "-" + resultado.getString("intervalo");
+                }
             } else {
                 intervalo = "fin";
             }
-            SqlLiteUtil.getInstance().desconectar();
+            MySqlUtil.getInstance().desconectar();
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
         return intervalo;
     }
-    public int getCantTurnos(ResultSet rs){
+    private int getCantTurnos(ResultSet rs){
         int rowCount = 0;  
         try {
             while (rs.next())  
@@ -190,11 +198,11 @@ public class Operaciones {
 
         DefaultTableModel model = (DefaultTableModel) tablaAgenda.getModel();
         model.setRowCount(0);
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
 
-        ResultSet res2 = SqlLiteUtil.getInstance().query("select * from turno where fecha ='" + fecha + "' and id_profesional ='" + id_profesional + "'");
+        ResultSet res2 = MySqlUtil.getInstance().query("select * from turno where fecha ='" + fecha + "' and id_profesional ='" + id_profesional + "'");
         int cant = getCantTurnos(res2);
-        ResultSet res = SqlLiteUtil.getInstance().query("select * from turno where fecha ='" + fecha + "' and id_profesional ='" + id_profesional + "'");
+        ResultSet res = MySqlUtil.getInstance().query("select * from turno where fecha ='" + fecha + "' and id_profesional ='" + id_profesional + "'");
         while (h_inicio < h_fin || fila < cant) {
             if (aux != 0) {
                 if (aux == 60) {
@@ -246,68 +254,66 @@ public class Operaciones {
             fila++;
             i++;
         }
-        SqlLiteUtil.getInstance().desconectar();
+        MySqlUtil.getInstance().desconectar();
     }
 
     public String getIdProfesional(String nombre, String mail) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado;
         try {
-            resultado = SqlLiteUtil.getInstance().query("select max(id_profesional) from profesional");
+            resultado = MySqlUtil.getInstance().query("select max(id_profesional) from profesional");
             String ret = resultado.getString(1);
-            SqlLiteUtil.getInstance().desconectar();
+            MySqlUtil.getInstance().desconectar();
             return ret;
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        MySqlUtil.getInstance().desconectar();
         return null;
     }
 
     public ResultSet getHorarios(String id_horario) {
-        SqlLiteUtil.getInstance().conectar();
         ResultSet resultado = null;
         String sql = "select * from horario where id_horario = '" + id_horario + "'";
-        resultado = SqlLiteUtil.getInstance().query(sql);
-        SqlLiteUtil.getInstance().desconectar();
+        resultado = MySqlUtil.getInstance().query(sql);
         return resultado;
     }
 
     public void eliminarProfesional(String id_profesional) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet res = null;
         String sql = "delete from profesional where id_profesional='" + id_profesional + "'";
-        res = SqlLiteUtil.getInstance().query(sql);
-        SqlLiteUtil.getInstance().desconectar();
+        res = MySqlUtil.getInstance().query(sql);
+        MySqlUtil.getInstance().desconectar();
     }
 
     public void eliminarHorario(String id_horario) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet res = null;
         String sql = "delete from horario where id_horario='" + id_horario + "'";
-        res = SqlLiteUtil.getInstance().query(sql);
-        SqlLiteUtil.getInstance().desconectar();
+        res = MySqlUtil.getInstance().query(sql);
+        MySqlUtil.getInstance().desconectar();
     }
 
     public boolean existeProfesional(String id_profesional) {
-        SqlLiteUtil.getInstance().conectar();
+        MySqlUtil.getInstance().conectar();
         ResultSet resultado;
 
         String sql = "select * from profesional where id_profesional='" + id_profesional + "'";
-        resultado = SqlLiteUtil.getInstance().query(sql);
+        resultado = MySqlUtil.getInstance().query(sql);
         try {
             if (resultado.next()) {
-                SqlLiteUtil.getInstance().desconectar();
+                MySqlUtil.getInstance().desconectar();
                 return true;
             } else {
-                SqlLiteUtil.getInstance().desconectar();
+                MySqlUtil.getInstance().desconectar();
                 return false;
 
             }
         } catch (SQLException ex) {
             Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        MySqlUtil.getInstance().desconectar();
         return false;
     }
 
@@ -320,6 +326,57 @@ public class Operaciones {
      * @return
      */
     public boolean validarUsuario(String nombre, String pass) {
-        return nombre.equals(SqlLiteUtil.getInstance().getUser()) && pass.equals(SqlLiteUtil.getInstance().getPass());
+        return nombre.equals(MySqlUtil.getInstance().getUser()) && pass.equals(MySqlUtil.getInstance().getPass());
     }
+    
+    public HashMap<String,Boolean> getDiasLaborales(String idHorario) throws SQLException{
+        HashMap<String,Boolean> resul = new HashMap<String,Boolean>();
+        MySqlUtil.getInstance().conectar();
+        ResultSet res = MySqlUtil.getInstance().query("select lunes>0 as lunes, martes>0 as martes, miercoles>0 as miercoles, jueves>0 as jueves, viernes>0 as viernes, sabado>0 as sabado, domingo>0 as domingo from tuno_intervalos where id_horario="+idHorario);
+        res.next();
+        resul.put("lunes", res.getDouble("lunes")   >0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("martes", res.getDouble("martes")>0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("miercoles", res.getDouble("miercoles")>0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("jueves", res.getDouble("jueves")>0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("viernes", res.getDouble("viernes")>0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("sabado", res.getDouble("sabado")>0?Boolean.TRUE:Boolean.FALSE);
+        resul.put("domingo", res.getDouble("domingo")>0?Boolean.TRUE:Boolean.FALSE);
+
+        MySqlUtil.getInstance().desconectar();
+        
+        return resul;
+    }
+    
+    public boolean hayTurno(String fecha, String nombreDia, String id_horario, String id_profesional){
+        try {
+            MySqlUtil.getInstance().conectar();
+
+            ResultSet res = MySqlUtil.getInstance().query("select * from tuno_intervalos where id_horario=" + id_horario);
+            
+                res.next();
+
+                double totales = res.getDouble(nombreDia);
+
+                if (totales > 0) {
+                    res = MySqlUtil.getInstance().query("select * from turno_dados where fecha like '" + fecha + "' and  id_profesional=" + id_profesional);
+                    if (res.next()) {
+                        int cantidad = res.getInt("dados");
+                        if (cantidad < totales) {
+                            MySqlUtil.getInstance().desconectar();
+                            return true;
+                        }
+                    } else {
+                        return true;
+                    }
+
+                }
+                MySqlUtil.getInstance().desconectar();
+           
+            return false;
+        } catch (SQLException ex) {
+            Logger.getLogger(Operaciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return false;    
+    }
+    
 }
